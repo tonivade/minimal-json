15bfb8c1f9ddc3f184fe8734a13cc6b8a0bb2778
Antonio Muñoz <antoniogmc@gmail.com>
2023-03-24 22:04:40 +0100

    remove all deprecated methods

--- com.eclipsesource.json/src/test/java/com/eclipsesource/json/JsonObject_Test.java
+++ com.eclipsesource.json/src/test/java/com/eclipsesource/json/JsonObject_Test.java
@@ -35,7 +35,6 @@ import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.Arrays;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
@@ -111,33 +110,6 @@ public class JsonObject_Test {
     JsonObject unmodifiableObject = JsonObject.unmodifiableObject(object);
 
     unmodifiableObject.add("foo", 23);
-  }
-
-  @Test
-  @SuppressWarnings("deprecation")
-  public void readFrom_reader() throws IOException {
-    assertEquals(new JsonObject(), JsonObject.readFrom(new StringReader("{}")));
-    assertEquals(new JsonObject().add("a", 23),
-                 JsonObject.readFrom(new StringReader("{ \"a\": 23 }")));
-  }
-
-  @Test
-  @SuppressWarnings("deprecation")
-  public void readFrom_string() {
-    assertEquals(new JsonObject(), JsonObject.readFrom("{}"));
-    assertEquals(new JsonObject().add("a", 23), JsonObject.readFrom("{ \"a\": 23 }"));
-  }
-
-  @Test(expected = ParseException.class)
-  @SuppressWarnings("deprecation")
-  public void readFrom_illegalJson() {
-    JsonObject.readFrom("This is not JSON");
-  }
-
-  @Test(expected = UnsupportedOperationException.class)
-  @SuppressWarnings("deprecation")
-  public void readFrom_wrongJsonType() {
-    JsonObject.readFrom("\"This is not a JSON object\"");
   }
 
   @Test
