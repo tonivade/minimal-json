49ac1ade78bd997e724717423793267e45b2b7d8
Antonio Muñoz <antoniogmc@gmail.com>
2024-05-05 16:57:16 +0200

    update copyright

--- com.eclipsesource.json/src/main/java/com/eclipsesource/json/Json.java
+++ com.eclipsesource.json/src/main/java/com/eclipsesource/json/Json.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2015, 2016 EclipseSource.
- * Copyright (c) 2023 Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
+ * Copyright (c) 2023-2024 Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
